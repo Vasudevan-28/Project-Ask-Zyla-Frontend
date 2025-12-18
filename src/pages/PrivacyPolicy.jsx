fddbc7028de527ba12e46ad98eb9eea88c3b52cf
Vasudevan K <teslastark03@gmail.com>
2025-12-18 23:03:26 +0530

    minor changes updates

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -72,7 +72,7 @@ export default function PrivacyPolicy() {
     },
     {
       title: "Contact Us",
-      content: ["📧 EMAIL ID: help@askzyla.com"],
+      content: ["📧 EMAIL ID: askzyla.zeaisoft@gmail.com"],
     },
   ];
 
